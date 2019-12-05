9185655c294ce154808c6ee704198ee138a5f573
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-12-05 16:51:24 +0100

    fix console log

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ console.log('student before the mutation', student);
 const studentWithSchool = student;
 studentWithSchool.school = 'Wild Code School';
 
-console.log('studentwithschool', student);
+console.log('studentWithSchool', student);
 console.log('student after the mutation', student);
 console.log(
   'Are student and studentWithSchool the same ?',
